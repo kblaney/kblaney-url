2d64f700d6ab5296726dacebf98881e9158ae2b2
Kyle Blaney <blaney.kyle@gmail.com>
2013-01-22 13:20:58 -0500

    Add initial version to source control.

---
+++ src/main/java/com/kblaney/xml/JtidyUrlToDomDocumentFunction.java
@@ -0,0 +1,77 @@
+package com.kblaney.xml;
+
+import com.kblaney.assertions.ArgAssert;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
+import org.w3c.dom.Document;
+import org.w3c.tidy.Tidy;
+
+/**
+ * Uses JTidy to convert a {@code java.net.URL} to an {@code org.w3c.dom.Document} with the contents of the URL.
+ */
+public final class JtidyUrlToDomDocumentFunction implements UrlToDomDocumentFunction
+{
+  private final int maxNumAttempts;
+
+  /**
+   * Constructs a new instance that attempts to download 10 times.
+   */
+  public JtidyUrlToDomDocumentFunction()
+  {
+    this(/*maxNumDownloadAttempts=*/10);
+  }
+
+  /**
+   * Constructs a new instance that attempts to download a specified maximum number of times.
+   * 
+   * @param maxNumDownloadAttempts the maximum number of download attempts, which must be positive
+   */
+  public JtidyUrlToDomDocumentFunction(final int maxNumDownloadAttempts)
+  {
+    this.maxNumAttempts = ArgAssert.assertGreaterThan(maxNumDownloadAttempts, 0, "maxNumAttempts");
+  }
+
+  @Override
+  public Document apply(final URL url) throws IOException
+  {
+    ArgAssert.assertNotNull(url, "url");
+
+    BufferedInputStream bufferedInputStream = null;
+    try
+    {
+      final Tidy tidy = new Tidy();
+      tidy.setXmlOut(true);
+      tidy.setQuiet(true);
+      tidy.setShowWarnings(false);
+      bufferedInputStream = new BufferedInputStream(getInputStream(url));
+      return tidy.parseDOM(bufferedInputStream, null);
+    }
+    finally
+    {
+      IOUtils.closeQuietly(bufferedInputStream);
+    }
+  }
+
+  private InputStream getInputStream(final URL url) throws IOException
+  {
+    IOException ioException = null;
+    int numAttempts = 0;
+    while (numAttempts < maxNumAttempts)
+    {
+      try
+      {
+        return url.openStream();
+      }
+      catch (final IOException e)
+      {
+        ioException = e;
+      }
+      numAttempts++;
+    }
+
+    throw ioException;
+  }
+}
